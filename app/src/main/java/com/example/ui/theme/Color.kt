9f4e94bad40ba987d203cfdab9efb30126ac389a
BYDXDM <1161467182@qq.com>
2026-07-30 10:44:16 +0800

    update app/src/main/java/com/example/ui/theme/Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Surface / appearance colors used in ConverterApp
+val AppSurfaceDark = Color(0xFF191C1E)
+val AppSurfaceDarkOverlay = Color(0x40000000) // 25% black overlay for backgrounds
